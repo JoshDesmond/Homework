1addd845d4fc8db9920673839b7977eae5765ba1
SkiingxMoose <jodesmond@live.com>
2015-08-30 19:58:16 -0400

    Set up the the basic comments and stuff

--- hwk1.rkt
+++ hwk1.rkt
@@ -1,4 +1,45 @@
 ;; The first three lines of this file were inserted by DrRacket. They record metadata
 ;; about the language level of this file in a form that our tools can easily process.
 #reader(lib "htdp-advanced-reader.ss" "lang")((modname hwk1) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #t #t none #f ())))
-;; Blah
+;; =====Homework Assigment 1=====
+;; Josh Desmond & Saahil Claypool
+;; ==============================
+;; Assignment instructions:
+;; http://web.cs.wpi.edu/~cs1102/a15/Assignments/Hwk1/index.html
+;; Homework Guidelines:
+;; http://web.cs.wpi.edu/~cs1102/a15/Assignments/grading-general.html
+
+
+
+
+#|
+Evaluate each of the following expressions by hand (use the rules covered in class, which match those of Beginner level). Show every step. In each expression, indicate the subexpression that is evaluated to obtain the next expression. For example:
+        (sqrt (+ (* 3 3) (* 4 4)))
+                 ^^^^^^^
+      = (sqrt (+ 9 (* 4 4)))
+                   ^^^^^^^
+      = (sqrt (+ 9 16))
+              ^^^^^^^^
+      = (sqrt 25)
+        ^^^^^^^^^
+      = 5
+If an expression would result in an error, show all of the steps up to the error, then indicate the error message you'd get (error messages don't need to be verbatim, as long as they convey the right kind of error). You can use the Stepper to check your answers, but do the problem manually first to make sure you understand how Racket works.
+
+8: (/ (- (* 9 3) (double 'a)) 2) where double is defined as (define (double n) (* n 2))
+
+9: (or (< 5 2) (and (= 15 (- 18 3)) (> 8 4)))
+
+10: (and (+ 5 -1) false)
+
+11: (apply-patch 'remove "this is a test string") [use your own apply-patch program from this assignment]
+
+Debugging Racket Programs
+
+For each of the following DrRacket error messages (from Beginner language level), describe what code that produces this error message would look like and provide a small illustrative example of code that would yield this error. Your description should not simply restate the error message!
+
+12: cond: expected a clause with a question and answer, but found a clause with only one part
+
+13: x: this variable is not defined
+
+14: function call: expected a function after an open parenthesis, but found a number
+|#
