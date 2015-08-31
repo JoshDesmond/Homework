5732cc4175708e371c4f452f8c4aadce8f88d064
SaahilClaypool <saahil@claypools.org>
2015-08-31 10:13:58 -0400

    made some awesome changes

--- hwk1.rkt
+++ hwk1.rkt
@@ -1,6 +1,6 @@
 ;; The first three lines of this file were inserted by DrRacket. They record metadata
 ;; about the language level of this file in a form that our tools can easily process.
-#reader(lib "htdp-advanced-reader.ss" "lang")((modname hwk1) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #t #t none #f ())))
+#reader(lib "htdp-advanced-reader.ss" "lang")((modname hwk1) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #t #t none #f () #f)))
 ;; =====Homework Assigment 1=====
 ;; Josh Desmond & Saahil Claypool
 ;; ==============================
@@ -9,8 +9,7 @@
 ;; Homework Guidelines:
 ;; http://web.cs.wpi.edu/~cs1102/a15/Assignments/grading-general.html
 
-
-
+(define (function b))
 
 #|
 Evaluate each of the following expressions by hand (use the rules covered in class, which match those of Beginner level). Show every step. In each expression, indicate the subexpression that is evaluated to obtain the next expression. For example:
