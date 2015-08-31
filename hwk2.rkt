a82d1cb37b50695dd46ff73bec46fd337eff3074
SkiingxMoose <jodesmond@live.com>
2015-08-30 20:10:30 -0400

    added second homework file

---
+++ hwk2.rkt
@@ -0,0 +1,10 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-advanced-reader.ss" "lang")((modname hwk2) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #t #t none #f ())))
+;; =====Homework Assigment 2=====
+;; Josh Desmond & Saahil Claypool
+;; ==============================
+;; Assignment instructions:
+;; http://web.cs.wpi.edu/~cs1102/a15/Assignments/Hwk2/index.html
+;; Homework Guidelines:
+;; http://web.cs.wpi.edu/~cs1102/a15/Assignments/grading-general.html
