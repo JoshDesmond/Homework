89da4aba5b0318b51f50e8d58305b2ee107c1b72
SkiingxMoose <jodesmond@live.com>
2015-09-02 19:35:31 -0400

    deleted test changes

--- hwk1.rkt
+++ hwk1.rkt
@@ -54,7 +54,8 @@
 (define (apply-op operation string position)
   (cond [(insert? operation) (string-append (string-append (substring string 0 position)
                                                            (insert-string operation))
-                                           (substring string position))]
+
+                                            (substring string position))]
         [(delete? operation)   (string-append (substring string 0 position) (substring string (+ position (delete-number operation))))]))
 (check-expect (apply-op INSERT-BLAH "abcdefg" 4) "abcdBLAHefg")
 (check-expect (apply-op (make-insert "Dopa") "DopaSeratonin" 4) "DopaDopaSeratonin")
@@ -68,64 +69,96 @@
   (apply-op (patch-operation a-patch) string (patch-position a-patch)))
  
 (check-expect (apply-patch PATCH-EXAMPLE "123456789") "1234BLAH56789")
- 
+
+
+;;changes to overlap branch
 ;; Overlap?: patch patch -> boolean
 ;; consumes 2 patches and determines if they overlap
- 
- 
- 
- 
- 
+(check-expect (overlap? (make-patch 4 INSERT-BLAH)(make-patch 4 INSERT-BLAH)) true) 
+(check-expect (overlap? (make-patch 4 DELETE3)(make-patch 4 DELETE3)) true) 
+(check-expect (overlap? (make-patch 4 INSERT-BLAH)(make-patch 4 DELETE3)) true) 
+
+(define (overlap? patchA patchB)
+  (cond [(and (insert? (patch-operation patchA))
+              (insert? (patch-operation patchB)))
+         (insertion-overlap? patchA patchB)]
+        [(and (delete? (patch-operation patchA))
+              (delete? (patch-operation patchB)))
+        (deletion-overlap? patchA patchB)]
+         
+          [else(mixed-overlap patchA patchB)]))
+
+
+
+
 ;; helper functions
 ;; Insertions-overlap?: patch patch -> boolean
-;; Both patches must have insertions as operations determines if they overlap
+;; Both patches must have insertions as operations determines if they overlap 
 (patch-position PATCH-EXAMPLE)
-(define (Insertion-overlap? patchA patchB)
+(define (insertion-overlap? patchA patchB)
   (= (patch-position patchA)
      (patch-position patchB)))
- 
- 
-(check-expect (Insertion-overlap? (make-patch 4 INSERT-BLAH)
+
+
+(check-expect (insertion-overlap? (make-patch 4 INSERT-BLAH)
                                   (make-patch 4 INSERT-BLAH )) true)
- 
-(check-expect (Insertion-overlap? (make-patch 8 INSERT-BLAH)
+
+(check-expect (insertion-overlap? (make-patch 8 INSERT-BLAH)
                                   (make-patch 0 INSERT-BLAH )) false)
- 
- 
+
+
+
 ;; getRight: patch (deletion) -> num
 ;; gets a deletion patch and determines right bound of range
 (check-expect (getRight (make-patch 4 (make-delete 3))) 7)
 (define (getRight a-patch)
   (+ (delete-number(patch-operation a-patch))
      (patch-position a-patch)))
- 
+
+
 ;;Deletion-overlap?: patch patch -> boolean
 ;; both patches myust have deletions as operations, determines if these overlap
-(define (Deletion-overlap? patchA patchB)
+(define (deletion-overlap? patchA patchB)
+
   (  and (<= (patch-position patchA) (getRight patchB))
           (>= (getRight patchA) (patch-position patchB))))
                                        
      (define DELETE3 (make-delete 3))
-     (check-expect (Deletion-overlap? (make-patch 5 DELETE3)
+
+     (check-expect (deletion-overlap? (make-patch 5 DELETE3)
                                       (make-patch 0 DELETE3))
                    false)
-     (check-expect (Deletion-overlap? (make-patch 0 DELETE3)
+     (check-expect (deletion-overlap? (make-patch 0 DELETE3)
                                       (make-patch 0 DELETE3))
                    true)
-     (check-expect (Deletion-overlap? (make-patch 2 DELETE3)
+     (check-expect (deletion-overlap? (make-patch 2 DELETE3)
                                       (make-patch 0 DELETE3))
                    true)
-    
+     
+
      #|  (check-expect (overlap? (make-patch INSERT-BLAH 4)
                         (make-patch INSERt-BlAH 4)) true)
 (check-expect (overlap? (make-patch INSERT-BLAH 0)
                         (make-patch-BLAH 8)) false )
 |#
+
+
+;; put mixed overlap here!
+(define (mixed-overlap patchA patchB)
+  true)
+
+
+
+
      #|(check-expect (overlap? (make-patch INSERT-BLAH 0)
                         (make-patch INSERT-BLAH 8)))
 (check-expect (overlap? (make-patch INSERT-BLAH 0)
-                        (make-patch INSERT-BLAH 8)))
+
+
+
+(make-patch INSERT-BLAH 8)))
 |#
+
 
 ;; Mixed-overlap?: patch patch -> boolean
 ;; Consumes a patch and a patch
@@ -149,8 +182,7 @@
 (define PATCH-INBLAH@2 (make-patch 2 INSERT-BLAH)) ;; Inserts BLAH at position 2 DELETE THESE
 ;; DELETE-5 deletes 5
 ;; DELETE-0 deletes 0
-;; Shwabang
-;; More changes
+
 
 ;; Consumes two patches and a string
 ;; Produces a string of the result, or false if the patches
@@ -166,7 +198,7 @@ string in the event of overlap?
  
  
 |#
-    
+
      
      
      #|
@@ -220,4 +252,7 @@ Josh
 Saa
 14: function call: expected a function after an open parenthesis, but found a number
 Josh
+
 |#
+     
+
